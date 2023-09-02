eed1f40d0da6259cac499033336a053ab0d61c46
Cairo <rcairor09@gmail.com>
2023-09-02 03:47:17 -0600

    cambios vistas principales

---
+++ GymTerminatorApp/wwwroot/js/layout.js
@@ -0,0 +1,14 @@
+﻿<script>
+    $(document).ready(function () {
+        // Manejar clics en los enlaces del menú
+        $(".nav-link").on("click", function (e) {
+            e.preventDefault(); // Evitar que se cargue la página completa
+
+            // Obtener la URL del enlace
+            var url = $(this).attr("href");
+
+            // Cargar el contenido de la página de forma asincrónica
+            $("#content-container").load(url);
+        });
+    });
+</script>
